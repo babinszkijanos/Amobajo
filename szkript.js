a7bafbeed615d59be3509c80e53fd3761d071958
babinszkijanos <babinszkijano@gmail.com>
2024-01-29 11:38:02 +0100

    F3

--- szkript.js
+++ szkript.js
@@ -3,4 +3,14 @@ function Asztal() {
     var row = table.insertRow(0);
     var cell= row.insertCell(0);
   }
-  
+
+  function createTable(numTables, numRows) {
+
+  const numTables = prompt("Írjon be számokat");
+  const numRows = prompt("Írjon be számokat");
+
+  if (numTables && numRows && !isNaN(numTables) && !isNaN(numRows)) {
+    createTable(parseInt(numTables), parseInt(numRows));} 
+    else {
+    alert("Kérem írjon be számokat.");
+}
