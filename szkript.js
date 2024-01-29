4c5c457d557630f4a33fb8fdaa0ce82ad53c2357
babinszkijanos <babinszkijano@gmail.com>
2024-01-29 13:57:38 +0100

    F7

--- szkript.js
+++ szkript.js
@@ -1,21 +1,3 @@
-function Asztal() {
-    var table = document.getElementById("Asztal");
-    var row = table.insertRow(0);
-    var cell= row.insertCell(0);
-  }
+function create
 
-  function createTable(numTables, numRows) {
-
-  const numTables = prompt("Írjon be számokat");
-  const numRows = prompt("Írjon be számokat");
-}
-
-     if (numTables, numRows) {}
-
-    else {
-    alert("Kérem írjon be számokat.");
-}
-
-function O(xCordinate, yCordinate)
-
-function X(xCordinate, yCordinate)
+const Gomb = document.getElementById('Gomb');
