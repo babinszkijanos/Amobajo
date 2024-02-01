383346b554cf9005f85daa0c161aa8ee63cfa0fb
babinszkijanos <babinszkijano@gmail.com>
2024-02-01 07:13:37 +0100

    F9

--- szkript.js
+++ szkript.js
@@ -1,5 +1,15 @@
-const Játék = document.getElementById('Játék');
 
 function Érték() {
     alert("Ez lesz az érték");
+  }
+
+  function Játék() {
+
+    var numRows = parseInt(document.getElementById("inputRows").value);
+    var numColumns = parseInt(document.getElementById("inputColumns").value);
+}
+
+function Mindkétfeladatvégrehajtása() {
+    Érték();
+    Játék();
   }
