95a01ac3764eb969f27a821e899504ce1d444a13
babinszkijanos <babinszkijano@gmail.com>
2024-02-01 08:25:58 +0100

    F12

--- szkript.js
+++ szkript.js
@@ -18,5 +18,11 @@ function Mindkétfeladatvégrehajtása() {
     var numRows = parseInt(document.getElementById("sorérték").value);
     var numColumns = parseInt(document.getElementById("oszlopérték").value);
     var table = document.createElement("table");
-   
+    for (var a = 0; a < numRows) {
+        var row = table.insertRow();
+        for (var b = 0; b < numColumns) {
+          var cell = row.insertCell();
+          var square = document.createElement("div");
+        }
+    }
 }
